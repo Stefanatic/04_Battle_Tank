d2baeb0d457d16b80e5857e190966e0e40b7b4ab
LENOVO-PC\Stefan <stefan-fuetterer@web.de>
2016-12-25 18:20:09 +0100

    148

---
+++ BattleTank/Source/BattleTank/Private/TankTracks.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankTracks.h"
+
+
+
+void UTankTracks::SetThrottle(float Throttle)
+{
+
+	//auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s Throttle %f"), *Name, Throttle);
+
+	//TODO Clamp throttle value
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
